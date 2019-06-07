093441e9310b4725cfad2332c924cbf16015e014
Brendan Sullivan <sullivanbt@ornl.gov>
2019-06-07 16:44:56 -0400

    add initial manul reduction files

---
+++ manual_reduction/mandi_parallel.py
@@ -0,0 +1,134 @@
+from __future__ import (absolute_import, division, print_function)
+import os
+import threading
+import time
+import pickle
+
+
+#-------------------------------------------------------------------parameters in this block need to be set.
+# this should point to the version of python you'd like to run.  Until the algorith is more stable, this will
+# probably be the nightly or a dev build (on analysis, you probably want mantidpythonnightly)
+python_command = '/SNS/users/ntv/workspace/mantid/release/bin/mantidpython' #On analysis, should be 'mantidpythonnightly'
+python_command = 'mantidpythonnightly'
+#python_command = '/home/ntv/workspace/mantid/release/bin/mantidpython' #On analysis, should be 'mantidpythonnightly'
+peaksFile = None
+UBFile = None
+# parameters for parallel runs
+outDir = '/SNS/users/ntv/Desktop/beta_lac_july2018_secondtxtal/' #Where we save results - should end with /
+max_processes = 3 #Largest number of runs to reduce at a time - typically limited by RAM.
+reduce_one_run_script = 'mandi_singleRun.py'
+eventFilesFormat = '/SNS/MANDI/IPTS-8776/nexus/MANDI_%i.nxs.h5' # %i will be replaced by the run number
+run_nums = range(9113,9117+1) #Which run numbers to analyze - must be python iterable (e.g. list, so you can use range)
+                              #If using range(), remember you add 1 to the final number you want processed.
+
+# Spherical integration parameters - should be close to reasonable if you want
+# a comparison with profile fitting
+rA = 0.020 #Inner radius for spherical integration
+rB = 0.021 #Inner radius for background shell for spherical integration
+rC = 0.023 #Outer radius for background shell for spherical integration
+
+# For peak finding, predicting, and indexing
+minD = 60 #Estimate of smallest unit cell side length
+maxD = 110 #Estimate of largest unit cell side length
+tol = 0.15 #Tolerance for indexing
+a, b, c = 73.3, 73.3, 99.0 #Unit cell lenghts in angstrom
+alpha, beta, gamma = 90, 90, 120 #Angles for unit cell in degrees
+predictPeaks = True #Boolean to predict peaks or not
+min_pred_dspacing = 1.8 #Lowest d to predict peaks to
+max_pred_dspacing = 15.0 #Highest d to predict peak to
+min_pred_wl = 2.0 #Low wavelength to predict peaks
+max_pred_wl = 4.0 #High wavelength to predict peaks
+
+# IntegratePeaksProfileFitting parameters
+ModeratorFile = '/SNS/users/ntv/integrate/bl11_moderatorCoefficients_2018.dat' #file with moderator coefficients
+StrongPeaksParamsFile = None#'/home/ntv/integrate/strongPeakParams_pthMulti.pkl' #string to pkl file containing strong peaks parameters
+DetCalFile = '/SNS/MANDI/shared/ProfileFitting/MANDI_June2018.DetCal'
+#DetCalFile = '/SNS/MANDI/shared/ProfileFitting/MANDI_Jan2019.DetCal'
+IntensityCutoff = 200 #Peaks with spherical intensity below IntensityCutoff are considered weak peaks
+EdgeCutoff = 3 #Peaks within EdgeCutoff of the edge will have their profiles taken from a strong peak
+# --(the parameters here probably don't need changing)--
+FracStop = 0.15 #Fraction of the peak maximum we use to define the peak volume
+MinpplFrac = 0.9 #Lower fraction of background estimate to check against expected TOF profile
+MaxpplFrac = 1.1 #High  fraction of background estimate to check against expected TOF profile
+DQMax = 0.15 #Maximum box side length to consider for fitting.
+#-------------------------------------------------------------------end of parameters block
+
+# Create a dictionary for parameters
+d = {}
+d['outDir'] = outDir
+d['max_processes'] = max_processes
+d['reduce_one_run_script'] = reduce_one_run_script
+d['eventFilesFormat'] = eventFilesFormat
+d['run_nums'] = run_nums
+d['rA'] = rA
+d['rB'] = rB
+d['rC'] = rC
+d['minD'] = minD
+d['maxD'] = maxD
+d['tol'] = tol
+d['ModeratorFile'] = ModeratorFile
+d['StrongPeaksParamsFile'] = StrongPeaksParamsFile
+d['IntensityCutoff'] = IntensityCutoff
+d['EdgeCutoff'] = EdgeCutoff
+d['FracStop'] = FracStop
+d['MinpplFrac'] = MinpplFrac
+d['MaxpplFrac'] = MaxpplFrac
+d['DQMax'] = DQMax
+d['predictPeaks'] = predictPeaks
+d['min_pred_dspacing'] = min_pred_dspacing
+d['max_pred_dspacing'] = max_pred_dspacing
+d['min_pred_wl'] = min_pred_wl
+d['max_pred_wl'] = max_pred_wl
+d['a'] = a
+d['b'] = b
+d['c'] = c
+d['alpha'] = alpha
+d['beta'] = beta
+d['gamma'] = gamma
+d['peaksFile'] = peaksFile
+d['UBFile'] = UBFile
+d['DetCalFile'] = DetCalFile
+
+
+#Define a class for threading (taken from ReduceSCD_Parallel.py) and set up parallel runs
+class ProcessThread ( threading.Thread ):
+    command = ""
+
+    def setCommand( self, command="" ):
+        self.command = command
+
+    def run ( self ):
+        print('STARTING PROCESS: ' + self.command)
+        os.system( self.command )
+
+
+python = python_command
+procList=[]
+index = 0
+for r_num in run_nums:
+    dictPath = outDir + 'fitParams_%i.pkl'%r_num
+    d['runNumber'] = r_num
+    pickle.dump(d, open(dictPath, 'wb'))
+    procList.append( ProcessThread() )
+    cmd = '%s %s %s' % (python, reduce_one_run_script, dictPath)
+    procList[index].setCommand( cmd )
+    index = index + 1
+
+#
+# Now create and start a thread for each command to run the commands in parallel,
+# starting up to max_processes simultaneously.
+#
+all_done = False
+active_list=[]
+while not all_done:
+    if  len(procList) > 0 and len(active_list) < max_processes :
+        thread = procList[0]
+        procList.remove(thread)
+        active_list.append( thread )
+        thread.start()
+    time.sleep(2)
+    for thread in active_list:
+        if not thread.isAlive():
+            active_list.remove( thread )
+    if len(procList) == 0 and len(active_list) == 0 :
+        all_done = True
---
+++ manual_reduction/mandi_singleRun.py
@@ -0,0 +1,113 @@
+from __future__ import (absolute_import, division, print_function)
+import os
+import sys
+import pickle
+from mantid.simpleapi import *
+
+
+def doIntegration(d):
+    runNumber = d['runNumber']
+    nxsFormat = d['eventFilesFormat']
+    rA = d['rA']
+    rB = d['rB']
+    rC = d['rC']
+    minD = d['minD']
+    maxD = d['maxD']
+    tol = d['tol']
+    moderatorFile = d['ModeratorFile']
+    outDir = d['outDir']
+    predictPeaks = d['predictPeaks']
+    min_pred_dspacing = d['min_pred_dspacing']
+    max_pred_dspacing = d['max_pred_dspacing']
+    min_pred_wl = d['min_pred_wl']
+    max_pred_wl = d['max_pred_wl']
+    StrongPeaksParamsFile = d['StrongPeaksParamsFile']
+    IntensityCutoff = d['IntensityCutoff']
+    EdgeCutoff = d['EdgeCutoff']
+    FracStop = d['FracStop']
+    MinpplFrac = d['MinpplFrac']
+    MaxpplFrac = d['MaxpplFrac']
+    DQMax = d['DQMax']
+    a = d['a']
+    b = d['b']
+    c = d['c']
+    alpha = d['alpha']
+    beta = d['beta']
+    gamma = d['gamma']
+    DetCalFile = d['DetCalFile']
+
+    outputFilenameTemplate = outDir+'%s_ws_%i_mandi_reduced.%s' #String with output file format.  %s will be
+
+    try:
+        event_ws = Load(Filename=nxsFormat%runNumber, OutputWorkspace='event_ws')
+        if DetCalFile is not None:
+            print('Loading DetCal file %s'%DetCalFile)
+            LoadIsawDetCal(InputWorkspace=event_ws, Filename=DetCalFile)
+        ConvertToMD(InputWorkspace='event_ws', QDimensions='Q3D', dEAnalysisMode='Elastic', Q3DFrames='Q_lab',
+                    OutputWorkspace='MDdata', MinValues='-5,-5,-5', MaxValues='5,5,5', MaxRecursionDepth=10)
+        peaks_ws = FindPeaksMD(InputWorkspace='MDdata', MaxPeaks=150, DensityThresholdFactor=500, OutputWorkspace='peaks_ws')
+        try:
+            FindUBUsingFFT(PeaksWorkspace='peaks_ws', MinD=minD, MaxD=maxD, Tolerance=tol, Iterations=10, DegreesPerStep=1.0)
+        except:
+            FindUBUsingLatticeParameters(PeaksWorkspace='peaks_ws', a=a, b=b, c=c, alpha=alpha, beta=beta,
+                                         gamma=gamma, NumInitial=50, Tolerance=tol,Iterations=1000)
+        IndexPeaks(PeaksWorkspace='peaks_ws')
+        """
+        #Set optimal crytal position. This is experimental still.
+        OptimizeCrystalPlacement(PeaksWorkspace=peaks_ws, ModifiedPeaksWorkspace=peaks_ws, FitInfoTable='t2',
+                                 AdjustSampleOffsets=True, OptimizeGoniometerTilt=False)
+        newSampPos = [mtd['t2'].column(1)[0], mtd['t2'].column(1)[1], mtd['t2'].column(1)[2]]
+        SetCrystalLocation(InputWorkspace=event_ws, OutputWorkspace=event_ws, NewX=newSampPos[0],
+                           NewY=newSampPos[1], NewZ=newSampPos[2])
+        MDdata = ConvertToMD(InputWorkspace='event_ws', QDimensions='Q3D', dEAnalysisMode='Elastic',
+                             Q3DFrames='Q_lab', OutputWorkspace='MDdata', MinValues='-5,-5,-5',
+                             MaxValues='5,5,5', MaxRecursionDepth=10)
+        np.savetxt('/SNS/users/ntv/Desktop/nak/%i_samppos.txt'%runNumber, newSampPos)
+        """ #End set optimal xtal position
+
+        mtd.remove('event_ws') #Free up memory
+        if predictPeaks:
+            print("PREDICTING peaks to integrate....")
+            peaks_ws = PredictPeaks(InputWorkspace=peaks_ws,
+                                    WavelengthMin=min_pred_wl, WavelengthMax=max_pred_wl,
+                                    MinDSpacing=min_pred_dspacing, MaxDSpacing=max_pred_dspacing,
+                                    ReflectionCondition='Primitive' )
+
+        IntegratePeaksMD(InputWorkspace='MDdata', PeakRadius=rA, BackgroundInnerRadius=rB, BackgroundOuterRadius=rC,
+                         PeaksWorkspace='peaks_ws', OutputWorkspace='peaks_ws', CylinderLength=0.4, PercentBackground=20,
+                         ProfileFunction='IkedaCarpenterPV')
+
+        #for i in range(peaks_ws.getNumberPeaks()):
+        #    peaks_ws.getPeak(i).setRunNumber(runNumber)
+
+        IntegratePeaksProfileFitting(OutputPeaksWorkspace='peaks_ws_out', OutputParamsWorkspace='params_ws', InputWorkspace='MDdata',
+                                     PeaksWorkspace='peaks_ws', ModeratorCoefficientsFile=moderatorFile, DQMax=DQMax,
+                                     MinpplFrac=MinpplFrac, MaxpplFrac=MaxpplFrac, FracStop=FracStop, EdgeCutoff=EdgeCutoff,
+                                     IntensityCutoff=IntensityCutoff, StrongPeakParamsFile=StrongPeaksParamsFile)
+
+        paramsFileName = outputFilenameTemplate%('params',runNumber,'nxs')
+        peaksFileName = outputFilenameTemplate%('peaks',runNumber,'integrate')
+        peaksPFFileName = outputFilenameTemplate%('peaks_profileFitted',runNumber,'integrate')
+        matFileName = outputFilenameTemplate%('UB',runNumber,'mat')
+        if os.path.isfile(paramsFileName):
+            os.remove(paramsFileName)
+        if os.path.isfile(peaksFileName):
+            os.remove(peaksFileName)
+        if os.path.isfile(peaksPFFileName):
+            os.remove(peaksPFFileName)
+        if os.path.isfile(peaksPFFileName):
+            os.remove(matFileName)
+
+        SaveNexus(InputWorkspace='params_ws', Filename=paramsFileName)
+        SaveIsawPeaks(InputWorkspace='peaks_ws', Filename=peaksFileName)
+        SaveIsawPeaks(InputWorkspace='peaks_ws_out', Filename=peaksPFFileName)
+        SaveIsawUB(InputWorkspace='peaks_ws', Filename=matFileName)
+
+    except:
+        raise
+
+
+if __name__ == '__main__':
+    dictPath = str(sys.argv[1])
+    d = pickle.load(open(dictPath, 'rb'))
+    doIntegration(d)
